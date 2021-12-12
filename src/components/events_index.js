2775bba671eaf0499694b73f6aeb3a5e123db993
dai03 <d.taniguchi0803@gmail.com>
2021-12-12 19:07:25 +0900

    implement events_index

---
+++ src/components/events_index.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import _ from 'lodash'
+
+import { readEvents } from '../actions' 
+
+// class App extends Component {
+//   render() {
+//     return(
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange={() => {console.log("I am clicked!")}}/>
+//       </React.Fragment>
+//     ); 
+//   }
+// }
+
+class EventsIndex extends Component {
+  componentDidMount() {
+    this.props.readEvents()
+  }
+
+  renderEvents() {
+    return _.map(this.props.events, event => (
+      <tr key={event.id}>
+        <td>{event.id}</td>
+        <td>{event.title}</td>
+        <td>{event.body}</td>
+      </tr>
+    ))
+  }
+
+  render() {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Body</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {this.renderEvents()}
+        </tbody>
+      </table>
+    )
+  }
+}
+
+// User.defaultProps = {
+//   age: 1
+// }
+
+const mapStateToProps = state => ({ events: state.events})
+const mapDispatchToProps = ({ readEvents })
+export default connect(mapStateToProps, mapDispatchToProps)(EventsIndex)
